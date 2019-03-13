3868a4a7e2929b54be7749c93d275a84fea7ae0b
JiaYi Zhang <bbfag3@gmail.com>
2019-03-13 01:05:40 -0700

    0.7655 on Titanic

--- Titanic/cmp.py
+++ Titanic/cmp.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-a = pd.read_csv("3-layer-dense-submission.csv")
-b = pd.read_csv("rdm-forest-submission.csv")
+a = pd.read_csv("cleaned_sub - 1.csv")
+b = pd.read_csv("cleaned_sub.csv")
 
-aa = a.values
-bb = b.values
-k=0
-for i in range(len(aa)):
-    if aa[i][1] != bb[i][1]:
-        k+=1
-print(k)
-print(k/len(aa))
+c = pd.concat([a,b],axis=1)
+print(c)
